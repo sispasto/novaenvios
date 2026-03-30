35565f7696d37ee55ac2c48731f00786cfc75f91
sispasto <sispasto@envia.co>
2026-03-30 10:20:57 -0500

    version

--- componentes/bienvenida.js
+++ componentes/bienvenida.js
@@ -50,7 +50,7 @@ class BienvenidaComponent extends HTMLElement {
       if (this.versionApp) {
         const versionLabel = this.querySelector("#version-label");
         if (versionLabel) {
-          versionLabel.textContent = ""; //`Asist v${this.versionApp}`;
+          versionLabel.textContent = `NovaEnvios v${this.versionApp}`;
         }
       }
       // Limpiar scripts anteriores en el contenedor
