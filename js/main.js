9d20d1044290b58a96b43f1f47618d5e4f2e1b47
sispasto <sispasto@envia.co>
2026-04-01 16:23:21 -0500

    actualizaciones cada 30 segundos

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ function iniciarAutoUpdateSW() {
       //console.log("🔄 Buscando actualización del SW...");
       swRegistration.update();
     }
-  }, 1800000); // detecta versiones cada 30 minutos (1800000 ms)
+  }, 30000); // detecta versiones cada 30 minutos (1800000 ms)
 }
 
 /* =========================
